260512dc1e7c9c5bc58a2a5ce0fe398975b56c0e
wuwenbao <572300808@qq.com>
2017-10-18 11:31:16 +0800

    添加注释

--- string.go
+++ string.go
@@ -2,6 +2,9 @@ package tool
 
 import "regexp"
 
+/**
+字符串截取
+*/
 func Substr(str string, start int, length int) string {
 	rs := []rune(str)
 	rl := len(rs)
@@ -28,6 +31,9 @@ func Substr(str string, start int, length int) string {
 	return string(rs[start:end])
 }
 
+/**
+验证手机号格式
+*/
 func CheckMobile(mobile string) bool {
 	regular := "/^13\\d{9}$|^14\\d{9}$|^15\\d{9}$|^17\\d{9}$|^18\\d{9}$/"
 	return regexp.MustCompile(regular).MatchString(mobile)
